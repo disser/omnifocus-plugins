f3deec64303f513c88d135931dfe54349bbdaa60
Dave Disser <disser@gmail.com>
2021-04-04 09:16:47 -0700

    Add deferNextWeekend, completeAwaitReply

---
+++ disser-defer-buttons.omnifocusjs/Resources/deferNextWeekend.js
@@ -0,0 +1,27 @@
+(() => {
+	let action = new PlugIn.Action(function (selection) {
+		selection.tasks.forEach(function (task) {
+			let now = new Date();
+			let cal = Calendar.current;
+			let one_day = new DateComponents();
+			one_day.day = 1;
+			if (task.deferDate === null || task.deferDate < now) {
+				task.deferDate = now;
+			}
+
+			// move forward one day
+			task.deferDate = cal.dateByAddingDateComponents(task.deferDate, one_day)
+			// move forward to Saturday
+			while (task.deferDate.getDay() !== 6) {
+				task.deferDate = cal.dateByAddingDateComponents(task.deferDate, one_day)
+			}
+			task.deferDate = cal.startOfDay(task.deferDate)
+		})
+	});
+
+	action.validate = function (selection) {
+		return (selection.tasks.length >= 1)
+	};
+
+	return action;
+})();
